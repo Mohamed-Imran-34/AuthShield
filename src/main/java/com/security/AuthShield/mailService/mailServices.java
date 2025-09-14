396eb3489c8b94f383f1accab5f6fb9b57c682dd
Mohamed Imran <a1b2c3d4w7x8y9z0@gmail.com>
2025-09-14 20:32:45 +0530

    Func  modified

--- src/main/java/com/security/AuthShield/mailService/mailServices.java
+++ src/main/java/com/security/AuthShield/mailService/mailServices.java
@@ -1,31 +1,38 @@
 package com.security.AuthShield.mailService;
 
+import brevo.ApiClient;
+import brevo.ApiException;
+import brevo.Configuration;
+import brevoApi.TransactionalEmailsApi;
+import brevoModel.SendSmtpEmail;
+import brevoModel.SendSmtpEmailSender;
+import brevoModel.SendSmtpEmailTo;
 import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.Collections;
+
 @Component
 public class mailServices {
-    @Autowired
-    private JavaMailSender mailSender;
+
 
     @Autowired
     private TemplateEngine templateEngine;
 
+    @Value("${apiKey}")
+    private String apiKey;
+
+
+
+
     public void verifyOtpMail(String toEmail,String username,String otp) throws MessagingException {
+        ApiClient apiClient = Configuration.getDefaultApiClient();
+        apiClient.setApiKey(apiKey);
 
-
-//        SimpleMailMessage mailMessage= new SimpleMailMessage();
-//        mailMessage.setFrom("AuthShield<a1b2c3d4w7x8y9z0@gmail.com>");
-//        mailMessage.setTo(toEmail);
-//        mailMessage.setSubject("Otp for email verification");
-//        mailMessage.setText("Welcome to AuthShield \n \n  Hi "+username +",  \n \n Otp for your email verification is "+ otp+ ".Kindly use this Otp to verify your email and this Otp is only valid for next 10 minutes. \n \n  If you didn't request for email verification please ignore this email. \n \n With Regards, \n AuthShield.");
-//        mailSender.send(mailMessage);
 
         Context context = new Context();
         context.setVariable("email",toEmail);
@@ -33,28 +40,29 @@ public class mailServices {
         context.setVariable("otp",otp);
 
         String msg = templateEngine.process("verify-otp",context);
-        MimeMessage message= mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message);
-        helper.setFrom("AuthShield<a1b2c3d4w7x8y9z0@gmail.com>");
-        helper.setTo(toEmail);
-        helper.setSubject("Otp for email verification");
-        helper.setText(msg,true);
-        mailSender.send(message);
 
+        TransactionalEmailsApi transactionalEmailsApi =  new TransactionalEmailsApi(apiClient);
+        SendSmtpEmail sendSmtpEmail = new SendSmtpEmail();
+        sendSmtpEmail.setSender(new SendSmtpEmailSender().email("a1b2c3d4w7x8y9z0@gmail.com").name("AuthShield"));
+        sendSmtpEmail.setTo(Collections.singletonList(new SendSmtpEmailTo().email(toEmail).name(username)));
+        sendSmtpEmail.setSubject("Email verification OTP");
+        sendSmtpEmail.textContent(msg);
 
-
-
+        try{
+            transactionalEmailsApi.sendTransacEmail(sendSmtpEmail);
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
 
     public void resetOtpEmail(String toEmail,String username,String otp) throws MessagingException {
-//        SimpleMailMessage mailMessage = new SimpleMailMessage();
-//        mailMessage.setFrom("AuthShield<a1b2c3d4w7x8y9z0@gmail.com>");
-//        mailMessage.setTo(toEmail);
-//        mailMessage.setSubject("Otp for Resetting Password");
-//        mailMessage.setText("AuthShield \n \n"+"Hi "+username+",\n \n Otp for resetting the password for your account "+toEmail+" is "+otp+".Kindly use this Otp to reset your account password and this Otp is only valid for next 10 minutes. \n \n If you didn't request for password resetting Otp please ignore this email.\n \n With Regards, \n AuthShield.");
-//        mailSender.send(mailMessage);
+
+
+        ApiClient apiClient = Configuration.getDefaultApiClient();
+        apiClient.setApiKey(apiKey);
+
 
         Context context = new Context();
         context.setVariable("email",toEmail);
@@ -62,13 +70,19 @@ public class mailServices {
         context.setVariable("otp",otp);
 
         String msg = templateEngine.process("reset-otp",context);
-        MimeMessage message= mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message);
-        helper.setFrom("AuthShield<a1b2c3d4w7x8y9z0@gmail.com>");
-        helper.setTo(toEmail);
-        helper.setSubject("Otp for Password Reset");
-        helper.setText(msg,true);
-        mailSender.send(message);
+
+        TransactionalEmailsApi transactionalEmailsApi =  new TransactionalEmailsApi(apiClient);
+        SendSmtpEmail sendSmtpEmail = new SendSmtpEmail();
+        sendSmtpEmail.setSender(new SendSmtpEmailSender().email("a1b2c3d4w7x8y9z0@gmail.com").name("AuthShield"));
+        sendSmtpEmail.setTo(Collections.singletonList(new SendSmtpEmailTo().email(toEmail).name(username)));
+        sendSmtpEmail.setSubject("OTP for password reset");
+        sendSmtpEmail.setHtmlContent(msg);
+
+        try{
+            transactionalEmailsApi.sendTransacEmail(sendSmtpEmail);
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
